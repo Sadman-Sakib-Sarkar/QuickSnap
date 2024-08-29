6cf088f4999df01787130037eabacfe6925fd411
Sadman Sakib Sarkar <jucse29.391@gmail.com>
2024-08-29 13:18:39 +0600

    snapgames code changed

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -9,8 +9,6 @@ namespace CardGames
         public static void LoadResources()
         {
             Bitmap cards;
-            cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
         }
 
 		/// <summary>
