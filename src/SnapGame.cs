bfbc9d182a0eb9993e653c20e33bfdedce209837
musfikussalihinsifat <musfikus.stu2019@juniv.edu>
2024-08-29 13:14:45 +0600

    snapgame changed

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,7 +10,7 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-	    SwinGame.LoadFontNamed ("GameFont", " ChunkFive-Regular.otf", 12); //
+	    
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
         }
 
